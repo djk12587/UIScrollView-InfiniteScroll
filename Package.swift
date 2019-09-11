338fcb61f054d8643d9c8305a4bd73c34c3b5957
Dan Koza <djk12587@gmail.com>
2019-09-10 22:05:49 -0500

    trying to get it to work for objc

--- Package.swift
+++ Package.swift
@@ -9,6 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "UIScrollView_InfiniteScroll",
+            type: .dynamic,
             targets: ["UIScrollView_InfiniteScroll"]),
     ],
     dependencies: [
